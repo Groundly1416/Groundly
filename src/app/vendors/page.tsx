ef1fbde2019cfd3cc7744192e1554793348933e1
Joey DeJesus <jdejesus1416@gmail.com>
2026-04-14 18:19:14 -0400

    Force dynamic rendering on vendor pages

--- src/app/vendors/page.tsx
+++ src/app/vendors/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import Navbar from '@/components/layout/Navbar';
 import Footer from '@/components/layout/Footer';
 import { vendors as vendorService } from '@/lib/services';
